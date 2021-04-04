e0ca71ad4b29c339d130dc21a2acd6c494b70af2
이수인 <silee@weedscomm.com>
2021-04-04 23:00:59 +0900

    date test

--- src/test/dateapi/DateTest.java
+++ src/test/dateapi/DateTest.java
@@ -1,8 +1,18 @@
 package test.dateapi;
 
+import java.util.Calendar;
+
 public class DateTest {
 
     public static void main(String[] args) {
+
+
+        Calendar time = Calendar.getInstance();
+
+        for (int i = 0; i < 100; i++) {
+            time.add(Calendar.MINUTE, 1);
+            System.out.println(time.getTime());
+        }
 
     }
 }
