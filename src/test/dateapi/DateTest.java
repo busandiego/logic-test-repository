f255705d1faf85a97752f9cf4e11455591e983b8
이수인 <silee@weedscomm.com>
2021-04-05 23:16:15 +0900

    시간 test

--- src/test/dateapi/DateTest.java
+++ src/test/dateapi/DateTest.java
@@ -1,6 +1,7 @@
 package test.dateapi;
 
 import java.util.Calendar;
+import java.util.Date;
 
 public class DateTest {
 
@@ -9,10 +10,40 @@ public class DateTest {
 
         Calendar time = Calendar.getInstance();
 
-        for (int i = 0; i < 100; i++) {
-            time.add(Calendar.MINUTE, 1);
-            System.out.println(time.getTime());
+//        for (int i = 0; i < 100; i++) {
+//            time.add(Calendar.MINUTE, 1);
+//            System.out.println(time.getTime());
+//        }
+        Date date = new Date();
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        date.setHours(0);
+        cal.add(Calendar.HOUR, -9 -6);
+        cal.add(Calendar.MINUTE, +1);
+        System.out.println(cal.getTime());
+        for (int i = 0; i < 720; i++) {
+            cal.add(Calendar.MINUTE, +1 + 2);
         }
 
+        System.out.println(cal.getTime());
+
+
+        Date date2 = new Date();
+        Calendar cal2 = Calendar.getInstance();
+        cal2.setTime(date2);
+        date2.setHours(0);
+        cal2.add(Calendar.HOUR, -9 -6);
+        cal2.add(Calendar.MINUTE, +1);
+        System.out.println("cal2: " + cal2.getTime());
+        for (int i = 0; i < 720; i++) {
+            cal2.add(Calendar.MINUTE, +1 + 2 * i);
+        }
+
+        System.out.println("반복후 cal2: " + cal2.getTime());
     }
+
+    //
+
+
+
 }
